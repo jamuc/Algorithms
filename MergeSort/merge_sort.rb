ae15bbbd7fc9fc2c7bf482a431eef433f5a91eb8
Jason Franklin <jason.b.franklin@gmail.com>
2018-04-02 18:28:24 +0200

    Implement merge sort

--- MergeSort/merge_sort.rb
+++ MergeSort/merge_sort.rb
@@ -1,5 +1,34 @@
+# Merge Sort is a divide and conquer algorithm from John von Neumann
+# It divides the array, sorts them recursively and then merges the arrays
+# back together again
 class MergeSort
   def sort(array)
-	return []
+	n = array.length
+	return array if array.length == 1
+
+	index = n/2
+	left = sort(array[0..index-1])
+	right = sort(array[index..-1])
+	return merge(left, right)
+  end
+
+  private def merge(left, right)
+	c = []
+	# While there are elements left in either array
+	# compare the first ones and sort into array C
+	loop do
+	  if left.first <= right.first
+		c << left.first
+		left.shift
+	  else
+		c << right.first
+		right.shift
+	  end
+
+	  break if left.empty? || right.empty?
+	end
+
+	return c.concat(left) if right.empty?
+	return c.concat(right) if left.empty?
   end
 end
--- MergeSort/merge_sort_spec.rb
+++ MergeSort/merge_sort_spec.rb
@@ -7,6 +7,12 @@ describe 'MergeSort' do
   let(:unsorted_array) { [5, 10, 25, -3, 15, 201, 6, 0, -44] }
 
   describe '#sort' do
+	let(:single_element_array) { [5] }
+	context 'when the array only has one element' do
+	  it 'returns the element as its own array' do
+		expect(subject.sort(single_element_array)).to eq [5]
+	  end
+	end
 	it 'sorts the elements in the array' do
 	  expect(subject.sort(unsorted_array)).to eq unsorted_array.sort
 	end
